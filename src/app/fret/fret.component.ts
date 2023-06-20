1749da89cd252b9074a1bb5fe2e0b2ea1dfcdb06
Cameron Simspon-Allsop <csimpsonallsop@technolog.com>
2023-06-20 12:57:38 +0100

    fret selection status now passed in via parent component

--- src/app/fret/fret.component.ts
+++ src/app/fret/fret.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output } from '@angular/core';
 import { getFretWidth, getNoteIndex, getNotes } from 'src/main';
 
 @Component({
@@ -13,9 +13,8 @@ export class FretComponent implements OnInit, OnChanges {
   @Input() tuning: number = 0;
   @Input() flats: boolean = false;
   @Input() showNotes: boolean = false;
-  @Input() resetToggle: boolean = false;
-  isSelected: boolean = false;
-  resetValue: boolean = false;
+  @Input() isSelected: boolean = false;
+  @Output() clickEvent = new EventEmitter();
 
   ngOnInit() {
     this.width = getFretWidth(this.fretNumber);
@@ -26,14 +25,9 @@ export class FretComponent implements OnInit, OnChanges {
     let notes = getNotes(this.flats);
 
     this.note = notes.get(noteIndex)!;
-
-    if (this.resetValue !== this.resetToggle) {
-      this.resetValue = this.resetToggle;
-      this.isSelected = false;
-    }
   }
 
   onClick() {
-    this.isSelected = !this.isSelected;
+    this.clickEvent.emit();
   }
 }
--- src/app/string/string.component.ts
+++ src/app/string/string.component.ts
@@ -1,22 +1,58 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
+
+export interface FretStatus {
+  index: number;
+  isSelected: boolean;
+}
 
 @Component({
   selector: 'app-string',
   templateUrl: './string.component.html',
   styleUrls: ['./string.component.css'],
 })
-export class StringComponent implements OnInit {
-  frets: Array<number> = [];
+export class StringComponent implements OnInit, OnChanges {
+  frets: FretStatus[] = []; //fret index, isSelected
   @Input() numberFrets: number = 0;
   @Input() tuning: number = 0;
   @Input() showNotes: boolean = false;
   @Input() showFlats: boolean = false;
   @Input() resetToggle: boolean = false;
+  resetValue: boolean = false;
 
   ngOnInit() {
     this.frets = [];
     for (let i = 0; i < this.numberFrets; i++) {
-      this.frets.push(i);
+      this.frets.push({
+        index: i,
+        isSelected: false,
+      });
+    }
+  }
+
+  ngOnChanges(): void {
+    if (this.resetValue !== this.resetToggle) {
+      this.reset();
+      this.resetValue = this.resetToggle;
+    }
+  }
+
+  reset() {
+    for (let i = 0; i < this.frets.length; i++) {
+      this.frets[i].isSelected = false;
+    }
+  }
+
+  onFretClick(index: number) {
+    for (let i = 0; i < this.frets.length; i++) {
+      if (i === index) {
+        this.frets[i].isSelected = !this.frets[i].isSelected;
+      }
     }
   }
 }
