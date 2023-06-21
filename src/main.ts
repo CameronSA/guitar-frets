4ab7ea716ef3c3231548835cac8b9ddd99781d9b
CameronSA <cammeronallsop95@hotmail.co.uk>
2023-06-21 12:11:43 +0100

    added firebase

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
 platformBrowserDynamic()
@@ -106,3 +108,19 @@ export function getHalfFretWidth(index: number, offset: number): string {
 function fretWidth(index: number): number {
   return 8.35 / (index / (index / 2 + 4) + 1);
 }
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDTHBt_vNcYV00XGNViOgCCHCMBcS8BfLA",
+  authDomain: "guitarfrets-a2909.firebaseapp.com",
+  projectId: "guitarfrets-a2909",
+  storageBucket: "guitarfrets-a2909.appspot.com",
+  messagingSenderId: "666269205630",
+  appId: "1:666269205630:web:13ae7d17a12fa3330fc42f",
+  measurementId: "G-YQ8MS5S70V"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
